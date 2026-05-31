86751e382eac4efbc23d819430bbd0667e6f3390
Ryslaw <ryslaw81@outlook.com>
2026-05-31 15:10:07 +0200

    Get-TextConfig, Get-BinaryConfig and Write-Config stub

--- ProxyScript.ps1
+++ ProxyScript.ps1
@@ -22,21 +22,53 @@ function Convert-HexStringToObject {}
 #>
 function Convert-BlobToObject {}
 
+<#
+    .DESCRIPTION
+    Converts a PS object to hex string
+#>
+function Convert-ObjectToHexString {}
+
+<#
+    .DESCRIPTION
+    Converts a PS object to an object ready to be put to the Windows registry as a REG_BINARY value
+#>
+function Convert-ObjectToBlob {}
 
 <#
     .DESCRIPTION
     Reads the content of text proxy settings from Windows registry
 #>
-function Read-TextConfig {}
+function Get-TextConfig {
+    [PSCustomObject]@{
+        AutoConfigUrl = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigUrl
+        ProxyEnable = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable
+        ProxyServer = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer
+        ProxyOverride = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyOverride
+    }
+}
 
 <#
     .DESCRIPTION
     Reads the content of binary proxy settings from Windows registry
 #>
-function Read-BinaryConfig {}
+function Get-BinaryConfig {
+    [PSCustomObject]@{
+        DefaultConnectionSettings = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections" -Name DefaultConnectionSettings
+        SavedLegacySettings = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections" -Name SavedLegacySettings
+    }
+}
 
 <#
     .DESCRIPTION
     Write new proxy config to the registry
 #>
-function Write-Config {}
+function Write-Config {
+    params (
+        $AutoConfigUrl,
+        $ProxyEnable,
+        $ProxyServer,
+        $ProxyOverride,
+        $DefaultConnectionSettings,
+        $SavedLegacySettings
+    )
+}
